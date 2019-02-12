00ceddf6aa03d13fafd48f01275f13bb715acd8a
Romain Levasseur <17664@csmrouen.net>
2019-02-12 12:11:19 +0100

    Magnifique boucle git réalisée avec un enthousiasme proche de l'hystérie collective

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+$message = "<h1>Je fais du PHP - Boucle <em>for</em></h1>";
+
+
+for ($i = 0 ; $i < 11; $i++) {
+    $message .="<li>" . $i . "</li>";
+};
+
+
+$message .= "</ul>";
+
+echo $message;
